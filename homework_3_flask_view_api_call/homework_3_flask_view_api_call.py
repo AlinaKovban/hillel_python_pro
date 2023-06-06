f65cdf291205790c398271b2215d0bc32f60fafd
AlinaKovban <alina.kovban@gmail.com>
2023-06-06 12:22:22 +0300

    Corrected code according to PEP8, added limit on generating students

--- homework_3_flask_view_api_call/homework_3_flask_view_api_call.py
+++ homework_3_flask_view_api_call/homework_3_flask_view_api_call.py
@@ -12,37 +12,42 @@ app = Flask(__name__)
 
 fake_data = Faker("UK")
 
+
 @app.route("/students")
 def generate_students():
-    counter = request.args.get("input_counter", "1000")
-    counter_int = int(counter)
+    counter = int(request.args.get("input_counter", "1000"))
     students = []
-    for _ in range(int(counter_int)):
-        birthday = fake_data.date_of_birth(minimum_age=18, maximum_age=60).strftime("%Y-%m-%d")
+
+    if counter > 1000:
+        return "Counter of students should be equal 1000 or less"
+
+    for _ in range(int(counter)):
+        birthday = fake_data.date_of_birth(minimum_age=18,
+                                           maximum_age=60).strftime("%Y-%m-%d")
         person = fake_data.first_name() + " " + fake_data.last_name() + " " + fake_data.email() + " " + fake_data.password() + " " + birthday
         students.append(person)
-        
+
     data_frame = pd.DataFrame(students)
     data_frame.to_csv("students.csv", index=False)
-    
+
     students_str = "<br>".join(students)
     return students_str
-    
 
-@app.route("/bitcoin_value")  
+
+@app.route("/bitcoin_value")
 def get_bitcoin_value():
     url = "https://bitpay.com/api/rates"
     data_from_url = requests.get(url, {})
     value_currency = data_from_url.json()
     currency = request.args.get("currency", "USD")
     convert = request.args.get("convert", "1")
-    
-    if data_from_url.status_code not in (HTTPStatus.OK,):
+
+    if data_from_url.status_code != HTTPStatus.OK:
         return Response(
             "ERROR: Something went wrong",
             status=data_from_url.status_code
         )
-    
+
     for rate in value_currency:
         if rate['code'] == currency:
             bitcoin_rate = float(rate['rate'])
@@ -51,12 +56,5 @@ def get_bitcoin_value():
         return f"Currency {currency} not found"
 
     converted_value = float(convert) * bitcoin_rate
-    
+
     return f"1 bitcoin in {currency} is {bitcoin_rate}. {convert} bitcoin(s) = {converted_value} {currency}."
-            
-    
-
-
-
-
-
