7e9efd9b8876e1ef3d0205391e8c81a9472c16ed
AlinaKovban <alina.kovban@gmail.com>
2023-07-24 13:01:56 +0300

    Added relations to existing models

---
+++ homework_11/homework_11.py
@@ -0,0 +1,12 @@
+def new_format(string):
+    number = int(string)
+    formatted_number = "{:,}".format(number).replace(",", ".")
+    return str(formatted_number)
+
+
+assert (new_format("1000000") == "1.000.000")
+assert (new_format("100") == "100")
+assert (new_format("1000") == "1.000")
+assert (new_format("100000") == "100.000")
+assert (new_format("10000") == "10.000")
+assert (new_format("0") == "0")
